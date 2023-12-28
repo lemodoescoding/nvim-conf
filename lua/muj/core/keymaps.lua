31325b1881dd7e08299cd91cbf905ea384c2dc42
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2023-12-28 08:13:50 +0700

    Added some changes to keymaps.lua

---
+++ lua/muj/core/keymaps.lua
@@ -0,0 +1,26 @@
+vim.g.mapleader = " "
+local keymap = vim.keymap
+
+-- basic and important keymaps
+
+keymap.set("i", "jk", "<ESC>")
+keymap.set("n", "<leader>w", ":w<CR>")
+keymap.set("n", "<leader>qq", ":qa!<CR>")
+keymap.set("n", "<leader>qw", ":wqa<CR>")
+
+-- window splitting
+keymap.set("n", "<leader>sv", "<C-w>v") -- vertical
+keymap.set("n", "<leader>sh", "<C-w>s") -- horizontal
+keymap.set("n", "<leader>se", "<C-w>=") -- split equally
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split
+
+-- tab
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- new
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- next
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- prev
+
+-- nvim-tree
+keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>")
+keymap.set("n", "<leader>ex", ":NvimTreeClose<CR>")
