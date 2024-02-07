8089858f5aae3490aa031d1bf76dd7d57b12f47f
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2024-02-07 22:27:29 +0700

    Minor changes before trying neo-tree

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 require("muj.core.options")
 require("muj.core.keymaps")
 require("muj.lazy")
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
--- lua/muj/plugins/nvim-tree.lua
+++ lua/muj/plugins/nvim-tree.lua
@@ -1,52 +1,46 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
-  },
-  config = function()
-    local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local nvimtree = require("nvim-tree")
+		-- change color for arrows in tree to light blue
+		vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		-- configure nvim-tree
+		nvimtree.setup({
+			view = {
+				width = 35,
+			},
+			-- change folder arrow icons
+			renderer = {
+				full_name = true,
+				group_empty = true,
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+				},
+			},
 
-    -- change color for arrows in tree to light blue
-    vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+		})
 
-    -- configure nvim-tree
-    nvimtree.setup({
-      view = {
-        width = 35,
-      },
-    -- change folder arrow icons
-      renderer = {
-        full_name = true,
-        group_empty = true,
-        icons = {
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-          },
-        },
-      },
+		-- nvim-tree custom keymap
+		local keymap = vim.keymap
 
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-    })
-
-    -- nvim-tree custom keymap
-    local keymap = vim.keymap
-
-    keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>")
-    keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>")
-    keymap.set("n", "<leader>ex", ":NvimTreeClose<CR>")
-
-  end,
+		keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>")
+		keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>")
+		keymap.set("n", "<leader>ex", ":NvimTreeClose<CR>")
+	end,
 }
