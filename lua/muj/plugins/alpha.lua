65e35224ea174540b733c2ad67d39be5e56f26d0
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2024-02-08 16:28:13 +0700

    Changed alpha.nvim theme to startify

--- lua/muj/plugins/alpha.lua
+++ lua/muj/plugins/alpha.lua
@@ -1,41 +1,30 @@
 return {
-	"goolord/alpha-nvim",
-	event = "VimEnter",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local alpha = require("alpha")
+    local startify = require("alpha.themes.startify")
 
-		-- Set header
-		dashboard.section.header.val = {
-			" __       __                      ",
-			"|  \\     /  \\                     ",
-			"| ▓▓\\   /  ▓▓__    __       __    ",
-			"| ▓▓▓\\ /  ▓▓▓  \\  |  \\     |  \\   ",
-			"| ▓▓▓▓\\  ▓▓▓▓ ▓▓  | ▓▓      \\▓▓   ",
-			"| ▓▓\\▓▓ ▓▓ ▓▓ ▓▓  | ▓▓     |  \\   ",
-			"| ▓▓ \\▓▓▓| ▓▓ ▓▓__/ ▓▓     | ▓▓__ ",
-			"| ▓▓  \\▓ | ▓▓\\▓▓    ▓▓     | ▓▓  \\",
-			" \\▓▓      \\▓▓ \\▓▓▓▓▓▓ __   | ▓▓\\▓▓",
-			"                     |  \\__/ ▓▓   ",
-			"                      \\▓▓    ▓▓   ",
-			"                       \\▓▓▓▓▓▓    ",
-		}
+    startify.section.header.val = {
+      [[  __       __                           ]],
+      [[ |  \\     /  \\                        ]],
+      [[ | ▓▓\\   /  ▓▓__    __       __        ]],
+      [[ |  ▓▓▓\\ /  ▓▓▓  \\  |  \\     |  \\   ]],
+      [[ | ▓▓▓▓\\  ▓▓▓▓ ▓▓  | ▓▓      \\▓▓      ]],
+      [[ | ▓▓\\▓▓ ▓▓ ▓▓ ▓▓  | ▓▓     |  \\      ]],
+      [[ | ▓▓ \\▓▓▓| ▓▓ ▓▓__/ ▓▓     | ▓▓__     ]],
+      [[ | ▓▓  \\▓ | ▓▓\\▓▓    ▓▓     | ▓▓  \\  ]],
+      [[ \\▓▓      \\▓▓ \\▓▓▓▓▓▓ __   | ▓▓\\▓▓  ]],
+      [[                     |  \\__/ ▓▓        ]],
+      [[                      \\▓▓    ▓▓        ]],
+      [[                       \\▓▓▓▓▓▓         ]],
+    }
 
-		-- Set menu
-		dashboard.section.buttons.val = {
-			dashboard.button("     e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
-			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fg", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("SPC  q", "  > Quit NVIM", "<cmd>qa<CR>"),
-		}
+    -- Send config to alpha
+    alpha.setup(startify.config)
 
-		-- Send config to alpha
-		alpha.setup(dashboard.opts)
-
-		-- Disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-	end,
+    -- Disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
 }
