455da396d174bc654078631cf5b321d1d38e8428
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2024-02-08 17:52:11 +0700

    Redesigned alpha config

--- lua/muj/plugins/alpha.lua
+++ lua/muj/plugins/alpha.lua
@@ -1,30 +1,39 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local alpha = require("alpha")
-    local startify = require("alpha.themes.startify")
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local startify = require("alpha.themes.startify")
 
-    startify.section.header.val = {
-      [[  __       __                           ]],
-      [[ |  \\     /  \\                        ]],
-      [[ | ▓▓\\   /  ▓▓__    __       __        ]],
-      [[ |  ▓▓▓\\ /  ▓▓▓  \\  |  \\     |  \\   ]],
-      [[ | ▓▓▓▓\\  ▓▓▓▓ ▓▓  | ▓▓      \\▓▓      ]],
-      [[ | ▓▓\\▓▓ ▓▓ ▓▓ ▓▓  | ▓▓     |  \\      ]],
-      [[ | ▓▓ \\▓▓▓| ▓▓ ▓▓__/ ▓▓     | ▓▓__     ]],
-      [[ | ▓▓  \\▓ | ▓▓\\▓▓    ▓▓     | ▓▓  \\  ]],
-      [[ \\▓▓      \\▓▓ \\▓▓▓▓▓▓ __   | ▓▓\\▓▓  ]],
-      [[                     |  \\__/ ▓▓        ]],
-      [[                      \\▓▓    ▓▓        ]],
-      [[                       \\▓▓▓▓▓▓         ]],
-    }
+		startify.section.header.val = {
+			[[                                   __                ]],
+			[[      ___     ___    ___   __  __ /\_\    ___ ___    ]],
+			[[     / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+			[[    /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+			[[    \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+			[[     \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+		}
 
-    -- Send config to alpha
-    alpha.setup(startify.config)
+		startify.section.top_buttons.val = {
+			startify.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			startify.button("f", "󰱼  Find File", "<cmd>Telescope find_files<CR>"),
+			startify.button("r", "󰁯  Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+			startify.button("q", "  Quit NVIM", "<cmd>qa<CR>"),
+		}
 
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+		startify.section.bottom_buttons.val = {}
+
+		startify.section.footer.val = {
+			{ type = "text", val = "github.com/lemodoescoding" },
+		}
+
+		-- Send config to alpha
+		alpha.setup(startify.config)
+
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+		vim.cmd([[autocmd User AlphaReady set showtabline=0]])
+		vim.cmd([[autocmd BufUnload <buffer> set showtabline=2]])
+	end,
 }
