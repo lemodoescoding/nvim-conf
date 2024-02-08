82b6ea7595f677a39001d7cee481721e93c194ed
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2024-02-08 21:47:38 +0700

    Added custom keymapping in the which-key plugin conf

--- lua/muj/core/keymaps.lua
+++ lua/muj/core/keymaps.lua
@@ -10,16 +10,16 @@ keymap.set("n", "<leader>qf", ":qa!<CR>")
 keymap.set("n", "<leader>qw", ":wqa<CR>")
 
 -- window splitting
-keymap.set("n", "<leader>sv", "<C-w>v") -- vertical
-keymap.set("n", "<leader>sh", "<C-w>s") -- horizontal
-keymap.set("n", "<leader>se", "<C-w>=") -- split equally
+keymap.set("n", "<leader>sv", "<C-w>v")     -- vertical
+keymap.set("n", "<leader>sh", "<C-w>s")     -- horizontal
+keymap.set("n", "<leader>se", "<C-w>=")     -- split equally
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split
 
 -- tab
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- new
+keymap.set("n", "<leader>to", ":tabnew<CR>")   -- new
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close
-keymap.set("n", "<leader>tn", ":tabn<CR>") -- next
-keymap.set("n", "<leader>tp", ":tabp<CR>") -- prev
+keymap.set("n", "<leader>tn", ":tabn<CR>")     -- next
+keymap.set("n", "<leader>tp", ":tabp<CR>")     -- prev
 
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
