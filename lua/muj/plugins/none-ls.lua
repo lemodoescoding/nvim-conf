39f50a10ae98b465e2493eb097766957f473b08d
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2024-05-18 08:20:48 +0700

    Fix on none-ls

--- lua/muj/plugins/none-ls.lua
+++ lua/muj/plugins/none-ls.lua
@@ -1,36 +1,38 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = { "nvimtools/none-ls-extras.nvim" },
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvimtools/none-ls-extras.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
 
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		null_ls.setup({
-			sources = {
-				require("none-ls.diagnostics.eslint_d"),
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				-- null_ls.builtins.formatting.phpcsfixer,
-				null_ls.builtins.formatting.phpcbf,
-				-- null_ls.builtins.diagnostics.eslint_d,
-			},
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    local event = "BufWritePre"
+    local async = event == "BufWritePost"
 
-			-- make auto-format on save (async)
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
-		})
+    null_ls.setup({
+      sources = {
+        -- require("none-ls.diagnostics.eslint_d"),
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        -- null_ls.builtins.formatting.phpcsfixer,
+        null_ls.builtins.formatting.phpcbf,
+      },
 
-		-- format shortcut keymap
-		-- vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+      -- make auto-format on save (async)
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+    })
+
+    -- format shortcut keymap
+    -- vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
--- lua/muj/plugins/treesitter.lua
+++ lua/muj/plugins/treesitter.lua
@@ -1,61 +1,50 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	event = {
-		"BufReadPre",
-		"BufNewFile",
-	},
-
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		"windwp/nvim-ts-autotag",
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
-
 	config = function()
 		local treesitter = require("nvim-treesitter.configs")
-
 		treesitter.setup({
-			highlight = {
-				-- enable = true,
+			ensure_installed = {
+				"c",
+				"cpp",
+				"css",
+				"dockerfile",
+				"gitcommit",
+				"gitignore",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"php",
+				"sql",
+				"tsx",
+				"typescript",
 			},
 
-			indent = { enable = true },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
 
 			autotag = { enable = true },
 
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-			},
+			indent = { enable = true },
 
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<C-space>",
+					init_selection = "<C-space>", -- set to `false` to disable one of the mappings
 					node_incremental = "<C-space>",
 					scope_incremental = false,
 					node_decremental = "<bs>",
 				},
 			},
 		})
-
-		require("ts_context_commentstring").setup({})
 	end,
 }
