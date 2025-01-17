ed7eefc2a471e4ce6dcfaac4de23435df159451e
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2025-01-17 16:07:11 +0700

    fix(lsp): fix intelephense bug not recogznizing root dir

--- lua/muj/lsp/handlers.lua
+++ lua/muj/lsp/handlers.lua
@@ -113,6 +113,9 @@ return {
 		lspconfig["intelephense"].setup({
 			capabilites = capabilities,
 			on_attach = on_attach,
+			root_dir = function()
+				return vim.loop.cwd()
+			end,
 		})
 
 		lspconfig["emmet_ls"].setup({
